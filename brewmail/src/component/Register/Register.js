c9ee2c664f4be527aa94f352507d1c966c8434be
chaselancaster <chaseblancaster@gmail.com>
2019-06-05 12:01:21 -0700

    Add Redirect to home page after submit on Register.js

--- brewmail/src/component/Register/Register.js
+++ brewmail/src/component/Register/Register.js
@@ -52,42 +52,48 @@ class Register extends Component {
     const { username, password, email, location } = this.state;
     return (
       <div>
-        <h1>This is the Register page</h1>
-        <form onSubmit={e => this.handleRegister(e)}>
-          <h4>Username</h4>
-          <input
-            type="text"
-            name="username"
-            value={username}
-            onChange={this.changeHandler}
-          />
-          <br />
-          <h4>Password</h4>
-          <input
-            type="text"
-            name="password"
-            value={password}
-            onChange={this.changeHandler}
-          />
-          <br />
-          <h4>Email</h4>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={this.changeHandler}
-          />
-          <br />
-          <h4>Location</h4>
-          <input
-            type="text"
-            name="location"
-            value={location}
-            onChange={this.changeHandler}
-          />
-          <br />
-          <button type="submit">Register</button>
-        </form>
+        {this.state.logged ? (
+          <Redirect to={"/"} />
+        ) : (
+          <div>
+            <h1>This is the Register page</h1>
+            <form onSubmit={e => this.handleRegister(e)}>
+              <h4>Username</h4>
+              <input
+                type="text"
+                name="username"
+                value={username}
+                onChange={this.changeHandler}
+              />
+              <br />
+              <h4>Password</h4>
+              <input
+                type="text"
+                name="password"
+                value={password}
+                onChange={this.changeHandler}
+              />
+              <br />
+              <h4>Email</h4>
+              <input
+                type="email"
+                name="email"
+                value={email}
+                onChange={this.changeHandler}
+              />
+              <br />
+              <h4>Location</h4>
+              <input
+                type="text"
+                name="location"
+                value={location}
+                onChange={this.changeHandler}
+              />
+              <br />
+              <button type="submit">Register</button>
+            </form>
+          </div>
+        )}
       </div>
     );
   }
