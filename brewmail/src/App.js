659f3a6473d55ede60bc6ea857eca0be7d02f002
chaselancaster <chaseblancaster@gmail.com>
2019-06-05 09:38:53 -0700

    Add initial structure in Landing.js and add Switch route to App.js

--- brewmail/src/App.js
+++ brewmail/src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from "react";
 import { Switch, Route, withRouter } from "react-router-dom";
 import "./App.css";
 
-import * as routes from './constants/routes'
+import * as routes from "./constants/routes";
 
 class App extends Component {
   render() {
     return (
-
-    )
+      <div>
+        <Switch>
+          <Route exact path={routes.LANDING} render={() => <Landing />} />
+        </Switch>
+      </div>
+    );
   }
 }
 
---
+++ brewmail/src/component/Landing/Landing.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the landing page</h1>
+      </div>
+    );
+  }
+}
