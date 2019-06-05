74e1812977f4ffb0ab310af6fca0d505a062a16c
chaselancaster <chaseblancaster@gmail.com>
2019-06-05 09:32:15 -0700

    import routes to App.js

--- brewmail/src/App.js
+++ brewmail/src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 import { Switch, Route, withRouter } from "react-router-dom";
 import "./App.css";
 
+import * as routes from './constants/routes'
+
 class App extends Component {
   render() {
     return (
-      
+
     )
   }
 }
