5680d82ecdd17fc11e57a188b15874c60ec042e3
chaselancaster <chaseblancaster@gmail.com>
2019-06-05 13:37:23 -0700

    Add NavLinks to NavBar.js

--- brewmail/src/component/NavBar/NavBar.js
+++ brewmail/src/component/NavBar/NavBar.js
@@ -2,3 +2,21 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 import * as routes from "../../constants/routes";
+
+const NavBar = ({ currentUser, doLogout }) => (
+  <div>
+    <NavLink exact to={routes.LANDING}>
+      Home
+    </NavLink>
+    <NavLink>Profile</NavLink>
+    {currentUser ? (
+      <span>
+        <button onClick={doLogout}>Logout</button>
+      </span>
+    ) : (
+      <NavLink exact to={routes.LOGIN}>
+        Login
+      </NavLink>
+    )}
+  </div>
+);
