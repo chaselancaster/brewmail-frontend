8b6907fd8344e37e10d60900ac1a5cd73026668e
chaselancaster <chaseblancaster@gmail.com>
2019-06-05 09:51:52 -0700

    Add input fields in Register.js

--- brewmail/src/component/Register/Register.js
+++ brewmail/src/component/Register/Register.js
@@ -1,2 +1,15 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
+
+class Register extends Component {
+  render() {
+    <div>
+      <form>
+        <input type="text" name="username" />
+        <input type="text" name="password" />
+        <input type="email" name="email" />
+        <input type="text" name="location" />
+      </form>
+    </div>;
+  }
+}
