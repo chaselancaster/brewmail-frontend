dbe1b7323df2186aa14a57cfc7cec84a75a9fc43
chaselancaster <chaseblancaster@gmail.com>
2019-06-08 09:18:28 -0700

    add initial map in MatchesResults.js

--- brewmail/src/component/MatchesResults/MatchesResults.js
+++ brewmail/src/component/MatchesResults/MatchesResults.js
@@ -5,6 +5,25 @@ class MatchesResults extends Component {
     return (
       <div>
         <h1>This is the matches results component</h1>
+        <div>
+          {this.props.users.map((user, i) => {
+            return (
+              <li key={i}>
+                <br />
+                <span>User: {user.username}</span>
+                <br />
+                <span>Cellar Count: {user.cellarBeer.length}</span>
+                <br />
+                <span># of Beer For Trade: {user.ftBeer.length}</span>
+                <br />
+                <span># of Beer ISO: {user.isoBeer.length}</span>
+                <br />
+                <button>View User's Beer</button>
+                <button>Trade</button>
+              </li>
+            );
+          })}
+        </div>
       </div>
     );
   }
