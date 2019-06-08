aa1fefc091ceef7cfdc8ba6be7b33c27151910d3
chaselancaster <chaseblancaster@gmail.com>
2019-06-08 09:02:42 -0700

    Add MatchesResults.js

---
+++ brewmail/src/component/MatchesResults/MatchesResults.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class MatchesResults extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the matches results component</h1>
+      </div>
+    );
+  }
+}
