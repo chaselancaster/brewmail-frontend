d418b33ed66b7cf8720a887e951a05b487c88695
chaselancaster <chaseblancaster@gmail.com>
2019-06-09 22:13:59 -0700

    Add EditUser

---
+++ brewmail/src/component/EditUser/EditUser.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class EditUser extends Component {
+  render() {
+    return (
+      <div>
+        <h1>User Edit Page</h1>
+      </div>
+    );
+  }
+}
+
+export default EditUser;
--- brewmail/src/component/Profile/Profile.js
+++ brewmail/src/component/Profile/Profile.js
@@ -5,6 +5,27 @@ class Profile extends Component {
     return (
       <div>
         <h1>Profile</h1>
+        <h2>Hello {this.props.currentUser.username}</h2>
+        <div>
+          <h3>Beers you are searching for</h3>
+          {this.props.currentUser.isoBeer.map((beer, i) => {
+            return (
+              <li key={i}>
+                <span>
+                  <img src={beer.label} />
+                </span>
+                <br />
+                <span>Name: {beer.beerName}</span>
+                <br />
+                <span>ABV: {beer.beerABV}%</span>
+                <br />
+                <span>Style: {beer.beerStyle}</span>
+                <br />
+                <span>Brewed by: {beer.breweryName}</span>
+              </li>
+            );
+          })}
+        </div>
       </div>
     );
   }
